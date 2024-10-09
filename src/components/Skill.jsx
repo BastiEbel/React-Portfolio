255357815b9db39ba764a426e77975af431c3c27
BastiEbel <sebastianebel@hotmail.de>
2024-10-09 22:43:02 +0200

    skill section finished

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -14,31 +14,51 @@ export default function Skill() {
   return (
     <div className="skill-container">
       <div className="spacer"></div>
-      <div className="icon-container">
-        <div className="icons">
-          <img src={js} alt="JS" />
-          <img src={git} alt="Git" />
-          <img src={scrum} alt="Scrum" />
-          <FaReact
-            style={{
-              color: "white",
-              width: "88px",
-              height: "100px",
-              gap: "10px",
-            }}
-          />
+      <div className="overall-skills">
+        <div className="icon-container">
+          <div className="icons">
+            <img src={js} alt="JS" />
+            <img src={git} alt="Git" />
+            <img src={scrum} alt="Scrum" />
+            <FaReact
+              style={{
+                color: "white",
+                width: "88px",
+                height: "100px",
+                gap: "10px",
+              }}
+            />
+          </div>
+          <div className="icons">
+            <img src={angular} alt="Angular" />
+            <img src={firebase} alt="Firebase" />
+            <img src={rest} alt="Rest API" />
+          </div>
+          <div className="icons">
+            <img src={ts} alt="TS" />
+            <img src={css} alt="CSS" />
+          </div>
+          <div className="icons">
+            <img src={html} alt="HTML" />
+          </div>
         </div>
-        <div className="icons">
-          <img src={angular} alt="Angular" />
-          <img src={firebase} alt="Firebase" />
-          <img src={rest} alt="Rest API" />
-        </div>
-        <div className="icons">
-          <img src={ts} alt="TS" />
-          <img src={css} alt="CSS" />
-        </div>
-        <div className="icons">
-          <img src={html} alt="HTML" />
+        <div className="skill-section">
+          <div className="skill">
+            <p>Skills</p>
+            <div
+              style={{
+                borderRadius: "5px",
+                border: "4px solid #1CDAE6",
+                width: "289px",
+                height: "0px",
+              }}
+            ></div>
+          </div>
+          <p className="section-p">
+            I have gained experience in building projects with various front end
+            technologies and concepts.
+          </p>
+          <button>Get in touch</button>
         </div>
       </div>
     </div>
