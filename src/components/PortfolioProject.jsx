a5a74d0effd4fd309c82836cfce8555e07c4e107
BastiEbel <sebastianebel@hotmail.de>
2024-10-22 22:32:25 +0200

    Portfolioproject change style for bigger screen sizes

--- src/components/PortfolioProject.jsx
+++ src/components/PortfolioProject.jsx
@@ -17,39 +17,41 @@ export default function PortfolioProject() {
         </p>
       </section>
       <img className="side-logo" src={blueshadow} alt="Blue Shadow" />
-      <div className="project-overview">
-        {project.map((singleProject) => (
-          <div className="project-container" key={singleProject.id}>
-            <img
-              className="project-image"
-              src={singleProject.img}
-              alt={singleProject.id}
-            />
-            <div className="project-hover">
-              <div className="btn-container">
-                <a
-                  className="btn-github"
-                  target="_blank"
-                  href={singleProject.links.github}
-                >
-                  Github
-                </a>
-                <a
-                  className="btn-site"
-                  target="_blank"
-                  href={singleProject.links.site}
-                >
-                  Live test
-                </a>
-              </div>
-              <div className="description-container">
-                <h4>{singleProject.title}</h4>
-                <span>{singleProject.description}</span>
-                <p>{singleProject.skills}</p>
+      <div className="project-section">
+        <div className="project-overview">
+          {project.map((singleProject) => (
+            <div className="project-container" key={singleProject.id}>
+              <img
+                className="project-image"
+                src={singleProject.img}
+                alt={singleProject.id}
+              />
+              <div className="project-hover">
+                <div className="btn-container">
+                  <a
+                    className="btn-github"
+                    target="_blank"
+                    href={singleProject.links.github}
+                  >
+                    Github
+                  </a>
+                  <a
+                    className="btn-site"
+                    target="_blank"
+                    href={singleProject.links.site}
+                  >
+                    Live test
+                  </a>
+                </div>
+                <div className="description-container">
+                  <h4>{singleProject.title}</h4>
+                  <span>{singleProject.description}</span>
+                  <p>{singleProject.skills}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       <img
         className="side-logo-green"
